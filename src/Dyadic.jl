2a86134b32861b5c30cef444f123787cadca95d3
Matthew Roughan <matthew.roughana@adelaide.edu.au>
2018-05-03 00:30:49 +0930

    added tests and made a couple of corrections

--- src/Dyadic.jl
+++ src/Dyadic.jl
@@ -1,15 +1,15 @@
 
-type Dyadic
-    num::Int64
-    log2den::Int64
-    function Dyadic(num::Integer, den::Integer)
-        tmp = log2(abs(den))
-        if isinteger(tmp)
-            return new( sign(den)*num, Int(tmp) )
-        else
-            error( DomainError() )
-        end
-    end
-end
-convert(Float64, x::Dyadic) = x.num / 2^(x.log2den)
+# type Dyadic
+#    num::Int64
+#    log2den::Int64
+#    function Dyadic(num::Integer, den::Integer)
+#        tmp = log2(abs(den))
+#        if isinteger(tmp)
+#            return new( sign(den)*num, Int(tmp) )
+#        else
+#            error( DomainError() )
+#        end
+#    end
+#end
+#convert(Float64, x::Dyadic) = x.num / 2^(x.log2den)
 
