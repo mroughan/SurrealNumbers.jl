177bea4ea1e1a1b14a62312fbabb67837f267d5c
Matthew Roughan <matthew.roughana@adelaide.edu.au>
2018-05-16 13:29:48 +0200

    added in routines for output to DAG, improved printing options, some more examples including a DOT graph of the dyadic tree

---
+++ test/dyadic_tree.jl
@@ -0,0 +1,75 @@
+# generate the dyadic tree, suitable for output to DOT
+using SurrealNumbers
+# var_args = "color=red"
+var_args = ""
+n=3; x_scale = 2.25
+n=4; x_scale = 5.00
+
+name_map = Dict{SurrealFinite, Integer}()
+
+file = "dyadic_tree_$n.dot"
+
+io = open(file, "w")
+println(io, "digraph dyadic_tree {")
+z = zero(SurrealFinite)
+m = 0
+name_map[z] = m
+var_args = "pos=\"0,0!\""
+surreal2node(io, z, 0, extra_args=var_args)
+println(io, "node_day_0 [shape=none, pos=\"$(x_scale*(n+0.5)),$(0)!\", label=\"Day 0\", fontsize=18]")
+
+for k=1:n
+    println(io, "node_day_$k [shape=none, pos=\"$(x_scale*(n+0.5)),$(k)!\", label=\"Day $k\", fontsize=18]")
+    
+    var_args = "pos=\"-$(x_scale*k),$(k)!\""
+    x = convert(SurrealFinite, -k)
+    m += 1
+    surreal2node(io, x, m, extra_args=var_args)
+    name_map[x] = m
+    println(" k = $k, x = $x")
+    tmp = name_map[x.R[1]]
+    println(io, "   node_$m:R -> node_$tmp;")
+
+    var_args = "pos=\"$(x_scale*k),$(k)!\""
+    x = convert(SurrealFinite, k)
+    m += 1
+    surreal2node(io, x, m, extra_args=var_args)
+    name_map[x] = m
+    println(" k = $k, x = $x")
+    tmp = name_map[x.L[1]]
+    println(io, "   node_$m:L -> node_$tmp;")
+
+    for f=2:k
+        for j=(f-2)*2^(k-f+1) + (1:2:2^(k-f+1))
+            x = convert(SurrealFinite, j // 2^(k-f+1))
+            var_args = "pos=\"$(x_scale*convert(Float64, x)),$(k)!\""
+            m += 1
+            surreal2node(io, x, m, extra_args=var_args)
+            name_map[x] = m
+            println(" k = $k, f = $f, x = $x")
+            tmp = name_map[x.L[1]]
+            println(io, "   node_$m:L -> node_$tmp;")
+            tmp = name_map[x.R[1]]
+            println(io, "   node_$m:R -> node_$tmp;")
+
+            x = convert(SurrealFinite, -j // 2^(k-f+1))
+            var_args = "pos=\"$(x_scale*convert(Float64, x)),$(k)!\""
+            m += 1
+            surreal2node(io, x, m, extra_args=var_args)
+            name_map[x] = m
+            println(" k = $k, f = $f, x = $x")
+            tmp = name_map[x.L[1]]
+            println(io, "   node_$m:L -> node_$tmp;")
+            tmp = name_map[x.R[1]]
+            println(io, "   node_$m:R -> node_$tmp;") 
+        end
+    end
+end
+
+println(io, "}")
+close(io)
+# run(`dot -n -Kfdp -Tpdf -O $file`) 
+run(`neato -Tpdf -O $file`) 
+run(`neato -Tsvg -O $file`) 
+
+
---
+++ test/test_io.jl
@@ -0,0 +1,37 @@
+using SurrealNumbers
+
+# MISTAKE! a1 = convert(SurrealFinite, "{1//2| \phi, 1}")
+
+a2 = convert(SurrealFinite, "{1//2 | 2,  1}")
+a3 = convert(SurrealFinite, "{-1//2 | {|\phi}}")
+a4 = convert(SurrealFinite, "{-1//2 | 2.0, 3}")
+
+X = read("test_surreals.dat", SurrealFinite, 4)
+
+
+a4 = convert(SurrealFinite, 13//16)
+a5 = convert(SurrealFinite, -2//16)
+io_test_file = "test_surreals_io.dat"
+io = open(io_test_file, "w")
+pf(io, a4)
+pf(io, a5)
+println(io, expand(a4))
+println(io, expand(a5))
+close(io)
+
+Y = read(io_test_file, SurrealFinite, 5)
+
+surreal2tex(a4)
+
+io_test_file = "test_surreals_io.tex"
+io = open(io_test_file, "w")
+surreal2tex(io, a4; level = 0)
+surreal2tex(io, a4; level = 1)
+surreal2tex(io, a4; level = 2)
+close(io)
+
+x = SurrealFinite([a4], [])
+surreal2tex(x; level = 0)
+surreal2tex(x; level = 1)
+surreal2tex(x; level = 2)
+
